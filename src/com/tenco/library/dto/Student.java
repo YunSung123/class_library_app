3655ab46e381fc40e763bc2ba60f962bfbef2afc
Your Name <fosem@gmail.com>
2026-04-07 08:48:16 +0900

    dto-fields

--- src/com/tenco/library/dto/Student.java
+++ src/com/tenco/library/dto/Student.java
@@ -1,5 +1,16 @@
 package com.tenco.library.dto;
 
+import lombok.*;
+
 /// 학생 데이터 그릇
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class Student {
+    private int id;
+    private String name;
+    private String studentId;
 }
